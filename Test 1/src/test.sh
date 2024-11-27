4b8f0d22d28311b22775a79aa99727798b38f6dc
ghostoninternet <daosyphuc288@gmail.com>
2024-11-27 16:52:25 +0700

    Update

---
+++ Test 1/src/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Initialize the diff file
+diff_file="../test/diff.txt"
+echo "Diff Results for All Tests" > "$diff_file"
+echo "==========================" >> "$diff_file"
+
+# Array of test files
+test_files=("example5.kpl")
+
+# Loop through each test file
+for i in "${!test_files[@]}"; do
+  test_file="${test_files[$i]}"
+  # Calculate the result file number (1-based index)
+  result_number=$((5))
+
+  # Run the scanner and redirect the output to a result file
+  ./parser "../test/$test_file" > "../test/result_self${result_number}.txt"
+
+  # # Compare the generated result with the expected result and append to diff file
+  # echo -e "\nDifferences for $test_file:" >> "$diff_file"
+  # echo "--------------------------" >> "$diff_file"
+  # diff "../test/result_self${result_number}.txt" "../test/result${result_number}.txt" >> "$diff_file"
+
+  # # Check if there are differences
+  # if diff "../test/result_self${result_number}.txt" "../test/result${result_number}.txt" > /dev/null; then
+  #   echo "Test $test_file passed"
+  # else
+  #   echo "Test $test_file failed. See diff.txt for details."
+  # fi
+done
